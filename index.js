cab495232f66d5778c518f169f7f0029fc8507e3
thombanks <thomas.banks@wearecube3.com>
2017-09-08 14:00:23 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const tabNavs = document.querySelectorAll('.tab-nav')
+const tabs = document.querySelectorAll('.tabbed-content article')
+tabNavs.forEach((tabNav) => {
+  tabNav.addEventListener('click', (e) => {
+    let target = e.currentTarget.getAttribute('data-target')
+    tabs.forEach((tab) => {
+      tab.setAttribute('data-state', 'not-visible')
+    })
+    document.querySelector(`article[data-target="${target}"]`).setAttribute('data-state', 'is-visible')
+    tabNavs.forEach((tabNav) => {
+      tabNav.setAttribute('data-state', 'not-active')
+    })
+    document.querySelector(`li[data-target="${target}"]`).setAttribute('data-state', 'is-active')
+  })
+})
